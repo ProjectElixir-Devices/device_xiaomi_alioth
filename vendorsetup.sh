f487a13a6e1e23c072d088146962268356bcc8ed
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2023-05-23 23:41:25 +0600

    alioth: Update kernel repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,7 +7,7 @@ rm -rf hardware/xiaomi
 git clone https://github.com/ProjectElixir-Devices/alioth_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
 git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_alioth vendor/xiaomi/alioth
 git clone https://github.com/ProjectElixir-Devices/alioth_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
-git clone --depth=1 https://github.com/EmanuelCN/kernel_xiaomi_sm8250 -b staging kernel/xiaomi/alioth
+git clone --depth=1 https://github.com/AbrarNoob/kernel_xiaomi_sm8250-1 -b staging kernel/xiaomi/alioth
 git clone --depth=1 https://github.com/EmanuelCN/zyc_clang-14 prebuilts/clang/host/linux-x86/ZyC-clang
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 git clone https://gitlab.com/AbrarNoob/android_vendor_xiaomi_camera vendor/xiaomi/camera
