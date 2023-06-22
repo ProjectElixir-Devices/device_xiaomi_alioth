96fc550f276192fbfc7563f329c0ee1c5a3edd7e
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2023-06-22 20:39:22 +0600

    alioth: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 rm -rf hardware/xiaomi
 rm -rf hardware/qcom-caf/sm8250/audio
+
 # clone
 git clone https://github.com/ProjectElixir-Devices/alioth_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
 git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_alioth vendor/xiaomi/alioth
@@ -13,4 +14,4 @@ git clone --depth=1 https://github.com/EmanuelCN/zyc_clang-14 prebuilts/clang/ho
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 git clone https://gitlab.com/AbrarNoob/android_vendor_xiaomi_camera vendor/xiaomi/camera
 git clone https://github.com/LineageOS/android_hardware_lineage_compat hardware/lineage/compat
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-20.0-caf-sm8250 hardware/qcom-caf/sm8250/audio
+git clone https://github.com/SuperiorOS/android_hardware_qcom_audio -b thirteen-caf-sm8250 hardware/qcom-caf/sm8250/audio
